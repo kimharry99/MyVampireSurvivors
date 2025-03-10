1e26c6f3f915dd6b02cea3681ac1e015d644826f
kimharry99 <kimharry9927@gmail.com>
2025-03-10 11:33:41 +0900

    Cleanup WaveManager to simplify wave handling

--- Source/MyVampireSurvivors/Waves/WaveManager.cpp
+++ Source/MyVampireSurvivors/Waves/WaveManager.cpp
@@ -5,9 +5,6 @@
 
 #include "MyVamSurLogChannels.h"
 #include "Waves/Wave.h"
-#include "Waves/WaveDataAsset.h"
-#include "Waves/WaveFactory.h"
-#include "Waves/WaveScheduleData.h"
 #include "Waves/WaveTriggerComponent.h"
 
 AWaveManager::AWaveManager(const FObjectInitializer& ObjectInitializer)
@@ -15,14 +12,6 @@ AWaveManager::AWaveManager(const FObjectInitializer& ObjectInitializer)
 {
 	WaveTriggerComponent = CreateDefaultSubobject<UWaveTriggerComponent>(TEXT("WaveTriggerComponent"));
 	WaveTriggerComponent->OnWaveTriggered.AddDynamic(this, &ThisClass::HandleWaveTriggered);
-}
-
-void AWaveManager::PostInitializeComponents()
-{
-	Super::PostInitializeComponents();
-
-	WaveFactory = NewObject<UWaveFactory>(this);
-	check(WaveFactory);
 }
 
 void AWaveManager::BeginPlay()
@@ -34,23 +23,11 @@ void AWaveManager::BeginPlay()
 		WaveTriggerComponent->SetWaveSchedule(WaveSchedule);
 		WaveTriggerComponent->BeginWaveSchedule();
 	}
-
-	//WavePeriod = WaveSchedule ? WaveSchedule->WavePeriod : 3.0f;
-
-	//CurrentWaveIndex = -1;
-	//UpdateUpcomingWaveData();
-	//TriggerUpcomingWave();
-	//SetPeriodTimer();
 }
 
 void AWaveManager::EndPlay(const EEndPlayReason::Type EndPlayReason)
 {
 	Super::EndPlay(EndPlayReason);
-
-	if(UWorld* World = GetWorld())
-	{
-		World->GetTimerManager().ClearTimer(WavePeriodTimerHandle);
-	}
 
 	for (AWave* Wave : TriggeredWaves)
 	{
@@ -71,78 +48,6 @@ float AWaveManager::GetTimeUntilNextWave() const
 {
 	check(WaveTriggerComponent);
 	return WaveTriggerComponent->GetTimeUntilNextWave();
-}
-
-bool AWaveManager::IsAllWavesTriggered() const
-{
-	if (!WaveSchedule)
-	{
-		UE_LOG(LogMyVamSur, Warning, TEXT("WaveSchedule is not set in IsAllWavesTriggered [%s]."), *GetPathNameSafe(this));
-		return false;
-	}
-
-	return CurrentWaveIndex >= WaveSchedule->SortedWaves.Num();
-}
-
-void AWaveManager::TriggerUpcomingWave()
-{
-	if (UpcomingWaveData == nullptr)
-	{
-		return;
-	}
-
-	if (WaveFactory == nullptr)
-	{
-		UE_LOG(LogMyVamSur, Warning, TEXT("Wave factory is not set"));
-		return;
-	}
-
-	AWave* UpcomingWave = WaveFactory->CreateWave(UpcomingWaveData);
-	UpcomingWave->OnWaveCleared.AddDynamic(this, &ThisClass::HandleWaveClear);
-	UpcomingWave->Trigger();
-	TriggeredWaves.Add(UpcomingWave);
-}
-
-void AWaveManager::UpdateUpcomingWaveData()
-{
-	if (!WaveSchedule)
-	{
-		UE_LOG(LogMyVamSur, Warning, TEXT("WaveSchedule is not set in UpdateUpcomingWaveData [%s]."), *GetPathNameSafe(this));
-		return;
-	}
-
-	++CurrentWaveIndex;
-	if (WaveSchedule->SortedWaves.IsValidIndex(CurrentWaveIndex))
-	{
-		UpcomingWaveData = WaveSchedule->SortedWaves[CurrentWaveIndex];
-	}
-	else
-	{
-		UpcomingWaveData = nullptr;
-	}
-}
-
-void AWaveManager::SetPeriodTimer()
-{
-	if (UWorld* World = GetWorld())
-	{
-		World->GetTimerManager().SetTimer(WavePeriodTimerHandle, this, &ThisClass::PostPeriodTimerComplete, WavePeriod);
-	}
-}
-
-void AWaveManager::PostPeriodTimerComplete()
-{
-	if (UWorld* World = GetWorld())
-	{
-		World->GetTimerManager().ClearTimer(WavePeriodTimerHandle);
-	}
-
-	if (!IsAllWavesTriggered())
-	{
-		TriggerUpcomingWave();
-		SetPeriodTimer();
-		UpdateUpcomingWaveData();
-	}
 }
 
 void AWaveManager::HandleWaveTriggered(AWave* Wave)
--- Source/MyVampireSurvivors/Waves/WaveManager.h
+++ Source/MyVampireSurvivors/Waves/WaveManager.h
@@ -7,18 +7,16 @@
 #include "WaveManager.generated.h"
 
 class AWave;
-class UWaveDataAsset;
-class UWaveFactory;
 class UWaveScheduleData;
 class UWaveTriggerComponent;
 
 /**
- * WaveManager_Deprecated handles ordered waves.
+ * WaveManager handles periodical event waves.
  *
  * It triggers an initial wave when the game starts.
  * After a cooldown period in the wave has passed, the class will trigger the next wave.
  */
-UCLASS()
+UCLASS(ClassGroup = "Wave")
 class MYVAMPIRESURVIVORS_API AWaveManager : public AActor
 {
 	GENERATED_BODY()
@@ -28,7 +26,6 @@ public:
 
 protected:
 	//~AActor Interface
-	virtual void PostInitializeComponents() override;
 	virtual void BeginPlay() override;
 	virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
 	//~End of AActor Interface
@@ -41,34 +38,11 @@ private:
 	UPROPERTY(VisibleAnywhere, Category = "Wave")
 	TObjectPtr<UWaveTriggerComponent> WaveTriggerComponent;
 
-	UPROPERTY()
-	TObjectPtr<UWaveFactory> WaveFactory;
-
 	UPROPERTY(EditDefaultsOnly, Category = "Wave")
 	TObjectPtr<const UWaveScheduleData> WaveSchedule;
 
-	UPROPERTY(Transient)
-	float WavePeriod;
-
-	UPROPERTY(Transient)
-	int CurrentWaveIndex = -1;
-
-	UPROPERTY()
-	TObjectPtr<const UWaveDataAsset> UpcomingWaveData = nullptr;
-
 	UPROPERTY()
 	TArray<TObjectPtr<AWave>> TriggeredWaves;
-
-	FTimerHandle WavePeriodTimerHandle;
-
-	bool IsAllWavesTriggered() const;
-
-	void TriggerUpcomingWave();
-	void UpdateUpcomingWaveData();
-	void SetPeriodTimer();
-
-	UFUNCTION()
-	void PostPeriodTimerComplete();
 
 	UFUNCTION()
 	void HandleWaveTriggered(AWave* Wave);
