7c364bf07187d7e2e52d10bbe7c6e7a287dae019
kimharry99 <kimharry9927@gmail.com>
2025-03-11 09:48:15 +0900

    Add comments in `AWave`

--- Source/MyVampireSurvivors/Waves/Wave.h
+++ Source/MyVampireSurvivors/Waves/Wave.h
@@ -12,6 +12,11 @@ struct FWaveActorsToSpawn;
 
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnWaveCleared, AWave*, ClearedWave);
 
+/**
+ * AWave is responsible for managing a wave of spawned actors in the game.
+ * It handles the spawning of enemies or other wave participants using a Spawner,
+ * tracks their lifecycle, and triggers events when the wave is cleared.
+*/
 UCLASS(ClassGroup = "Wave")
 class MYVAMPIRESURVIVORS_API AWave : public AActor
 {
@@ -21,6 +26,10 @@ public:
 	AWave(const FObjectInitializer& ObjectInitializer);
 
 public:
+	/**
+	 * Triggers the wave by spawning a set of actors as defined in WaveSpawningActors.
+	 * Each actor is spawned using the Spawner and tracked if it implements the IWaveParticipantInterface.
+	 */
 	void Trigger(const TArray<FWaveActorsToSpawn>& WaveSpawningActors);
 
 	/** Delegate that is broadcasted when the wave is cleared. */
